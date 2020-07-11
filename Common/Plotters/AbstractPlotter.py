355ebeac0a1ba36bf553ae7d5bc0638962a805c6
enriqueescobar-askida <enrique.escobar@askida.com>
2020-07-11 18:43:22 -0400

    updater

--- Common/Plotters/AbstractPlotter.py
+++ Common/Plotters/AbstractPlotter.py
@@ -1,23 +1,13 @@
 from abc import ABC
 import pandas as pd
-from pyarrow.lib import null
 from Common.Measures.Time.TimeSpan import TimeSpan
 
 
 class AbstractPlotter(ABC):
-    _dataFrame: pd.DataFrame
-    _src: str
-    _draw_col: str
-    _legend_place: str
-    _ticker: str
-    _timeSpan: TimeSpan
+    __dataFrame: pd.DataFrame
+    __src: str
+    __Col: str
+    __legendPlace: str
+    __ticker: str
+    __timeSpan: TimeSpan
 
-    def __init__(self, df: pd.DataFrame, src: str = 'yahoo', tick: str = 'CNI', ts: TimeSpan = null):
-        self._dataFrame = df
-        self._src = src
-        self._legend_place = 'upper left'
-        self._ticker = tick
-        self._timeSpan = ts
-
-    def Plot(self):
-        pass
