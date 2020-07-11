69df8b6ab37ef7802320d2fe0b2af5b087981734
enriqueescobar-askida <enrique.escobar@askida.com>
2020-07-11 16:14:12 -0400

    test

---
+++ script-HistoricalPlotter.py
@@ -0,0 +1,7 @@
+from Common.Plotters.HistoricalPlotter import HistoricalPlotter
+from Common.StockOptions.Yahoo.YahooStockOption import YahooStockOption
+
+yahooStockOption: YahooStockOption = YahooStockOption('TD')
+print(yahooStockOption.HistoricalData.describe(include='all'))
+yahooStockOptionPlotter: HistoricalPlotter = HistoricalPlotter(yahooStockOption).Plot()
+yahooStockOptionPlotter.show()
