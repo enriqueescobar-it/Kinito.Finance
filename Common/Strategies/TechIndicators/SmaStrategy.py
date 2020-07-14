d475092bc6b1ae8aca67beec9ee1988a1120e0de
enriqueescobar-askida <enrique.escobar@askida.com>
2020-07-14 00:50:29 -0400

    update

---
+++ Common/Strategies/TechIndicators/SmaStrategy.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+from Common.Strategies.TechIndicators.AbstractTechIndicatorStrategy import AbstractTechIndicatorStrategy
+
+
+class SmaStrategy(AbstractTechIndicatorStrategy):
+    pass
