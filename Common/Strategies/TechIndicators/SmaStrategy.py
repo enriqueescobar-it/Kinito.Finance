f15afcf74722a5cf4b1a05b50cf17a1043b3ae61
enriqueescobar-askida <enrique.escobar@askida.com>
2020-07-15 02:27:24 -0400

    update

--- Common/Strategies/TechIndicators/SmaStrategy.py
+++ Common/Strategies/TechIndicators/SmaStrategy.py
@@ -24,13 +24,40 @@ class SmaStrategy(AbstractTechIndicatorStrategy):
         self._DataFrame[sma_indicator._Label + '009'] = sma_indicator._SMA009
         self._DataFrame[sma_indicator._Label + '010'] = sma_indicator._SMA010
         self._DataFrame[sma_indicator._Label + '020'] = sma_indicator._SMA020
+        self.__SMA20label = sma_indicator._Label + '020'
         self._DataFrame[sma_indicator._Label + '050'] = sma_indicator._SMA050
+        self.__SMA50label = sma_indicator._Label + '050'
         self._DataFrame[sma_indicator._Label + '100'] = sma_indicator._SMA100
         self._DataFrame[sma_indicator._Label + '200'] = sma_indicator._SMA200
-        '''buyNsellTuple = self.__buyNsell()
+        buyNsellTuple = self.__buyNsell()
         self._DataFrame[self._BuyLabel] = buyNsellTuple[0]
-        self._DataFrame[self._SellLabel] = buyNsellTuple[1]'''
-        print(self._DataFrame.head())
+        self._DataFrame[self._SellLabel] = buyNsellTuple[1]
+        print(self._DataFrame)
 
     def __buyNsell(self):
-        pass
+        buySignal = []
+        sellSignal = []
+        flag = -1
+
+        for i in range(len(self._DataFrame)):
+            if self._DataFrame[self.__SMA20label][i] > self._DataFrame[self.__SMA50label][i]:#
+                if flag != 1:
+                    buySignal.append(self._DataFrame[self.__ticker][i])
+                    sellSignal.append(np.nan)
+                    flag = 1
+                else:
+                    buySignal.append(np.nan)
+                    sellSignal.append(np.nan)
+            elif self._DataFrame[self.__SMA20label][i] < self._DataFrame[self.__SMA50label][i]:#
+                if flag != 0:
+                    buySignal.append(np.nan)
+                    sellSignal.append(self._DataFrame[self.__ticker][i])
+                    flag = 0
+                else:
+                    buySignal.append(np.nan)
+                    sellSignal.append(np.nan)
+            else:
+                buySignal.append(np.nan)
+                sellSignal.append(np.nan)
+
+        return buySignal, sellSignal
