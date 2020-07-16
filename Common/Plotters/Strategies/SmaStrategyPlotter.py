26f004cb85da7498193282760e3874d2ce57c2b2
enriqueescobar-askida <enrique.escobar@askida.com>
2020-07-16 02:03:55 -0400

    update

--- Common/Plotters/Strategies/SmaStrategyPlotter.py
+++ Common/Plotters/Strategies/SmaStrategyPlotter.py
@@ -18,6 +18,17 @@ class SmaStrategyPlotter(AbstractStrategyPlotter):
 
     def Plot(self):
         plt.figure(figsize=(self.__timeSpan.MonthCount / 2, 4.5))
+        plt.plot(self.__smaStrategy._DataFrame[self.__ticker], label=self.__Label, alpha=0.7)
+        plt.plot(self.__smaStrategy._DataFrame[self.__smaStrategy._Label + '005'], label=self.__smaStrategy._Label + '005', alpha=0.50, color='lightblue')
+        plt.plot(self.__smaStrategy._DataFrame[self.__smaStrategy._Label + '009'], label=self.__smaStrategy._Label + '009', alpha=0.50, color='lightgray')
+        plt.plot(self.__smaStrategy._DataFrame[self.__smaStrategy._Label + '010'], label=self.__smaStrategy._Label + '010', alpha=0.50, color='green')
+        plt.plot(self.__smaStrategy._DataFrame[self.__smaStrategy._Label + '020'], label=self.__smaStrategy._Label + '020', alpha=0.50, color='orange')
+        plt.plot(self.__smaStrategy._DataFrame[self.__smaStrategy._Label + '030'], label=self.__smaStrategy._Label + '030', alpha=0.50, color='yellow')
+        plt.plot(self.__smaStrategy._DataFrame[self.__smaStrategy._Label + '050'], label=self.__smaStrategy._Label + '050', alpha=0.50, color='pink')
+        plt.plot(self.__smaStrategy._DataFrame[self.__smaStrategy._Label + '100'], label=self.__smaStrategy._Label + '100', alpha=0.50, color='red')
+        plt.plot(self.__smaStrategy._DataFrame[self.__smaStrategy._Label + '200'], label=self.__smaStrategy._Label + '200', alpha=0.50, color='violet')
+        plt.scatter(self.__dateTimeIndex, self.__smaStrategy._DataFrame[self.__smaStrategy._BuyLabel], label=self.__smaStrategy._BuyLabel, marker='^', color='green')
+        plt.scatter(self.__dateTimeIndex, self.__smaStrategy._DataFrame[self.__smaStrategy._SellLabel], label=self.__smaStrategy._SellLabel, marker='v', color='red')
         plt.title(self.__Label + ' ' + self.__smaStrategy._Col + ' History ' + self.__smaStrategy._Label + ' BUY & SELL Signals')
         plt.xlabel(self.__timeSpan.StartDateStr + ' - ' + self.__timeSpan.EndDateStr)
         plt.xticks(rotation=45)
