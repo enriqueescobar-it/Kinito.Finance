570dea03b2057a7dd363edc3703d543121dd54d2
enriqueescobar-askida <enrique.escobar@askida.com>
2020-07-17 22:00:33 -0400

    update

--- script-Rsi.py
+++ script-Rsi.py
@@ -1,5 +1,7 @@
+# from Common.Plotters.Strategies.RsiStrategyPlotter import RsiStrategyPlotter
 from Common.Plotters.TechIndicators.RsiIndicatorPlotter import RsiIndicatorPlotter
 from Common.StockOptions.Yahoo.YahooStockOption import YahooStockOption
+# from Common.Strategies.TechIndicators.RsiStrategy import RsiStrategy
 from Common.TechIndicators.RsiIndicator import RsiIndicator
 
 yahooStockOption: YahooStockOption = YahooStockOption('CNI')
