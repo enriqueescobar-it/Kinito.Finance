38c5f88b8e365555818b45c250fefe5671f3c806
enriqueescobar-askida <enrique.escobar@askida.com>
2020-08-18 16:29:06 -0400

    update

--- Common/Comparators/Index/IndexComparator.py
+++ Common/Comparators/Index/IndexComparator.py
@@ -2,6 +2,7 @@ from Common.Comparators.Index.AbstractIndexComparator import AbstractIndexCompar
 from Common.StockOptions.Yahoo.YahooStockOption import YahooStockOption
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 
 class IndexComparator(AbstractIndexComparator):
@@ -21,13 +22,14 @@ class IndexComparator(AbstractIndexComparator):
         self.DataComparator = self.DataComparator.merge(df, left_index=True, right_index=True)
         self.DataNorma = (self.DataComparator - self.DataComparator.min()) / (
                     self.DataComparator.max() - self.DataComparator.min())
+        self.DataNorma = self.DataComparator / self.DataComparator.iloc[0]
         print(self.DataNorma.head())
-        plt.figure(figsize=(stock_option.TimeSpan.MonthCount/2, 4.5))
+        plt.figure(figsize=(10*math.log(stock_option.TimeSpan.MonthCount), 4.5))
         for c in self.DataNorma.columns.values:
           plt.plot(self.DataNorma.index, self.DataNorma[c], lw= 2, label = c)
 
-        #plt.title('_'.join(stockSymbols) + ' Cumulative Returns ' + str(num_months) + ' mts')
-        #plt.xlabel(start_date_s + ' - ' + end_date_s)
+        plt.title(stock_option.SourceColumn + ' Normalized ' + str(stock_option.TimeSpan.MonthCount) + ' months')
+        plt.xlabel(stock_option.TimeSpan.StartDateStr + ' - ' + stock_option.TimeSpan.EndDateStr)
         plt.ylabel('Growth per dollar invested')
-        #plt.legend(loc = legend_place, fontsize = 10)
+        plt.legend(loc = 'upper left', fontsize = 10)
         plt.show()
