6b8866b8ab6aefb99f5b1525bb89f8522115956b
enriqueescobar-askida <enrique.escobar@askida.com>
2020-08-19 00:23:50 -0400

    corr

--- Common/Comparators/Index/IndexComparator.py
+++ Common/Comparators/Index/IndexComparator.py
@@ -1,7 +1,9 @@
 from Common.Comparators.Index.AbstractIndexComparator import AbstractIndexComparator
 from Common.StockOptions.Yahoo.YahooStockOption import YahooStockOption
+from sklearn import preprocessing
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import math
 
 
@@ -10,6 +12,7 @@ class IndexComparator(AbstractIndexComparator):
     __indexList: list
     DataComparator: pd.DataFrame
     DataNorma: pd.DataFrame
+    DataScaled: pd.DataFrame
 
     def __init__(self, stock_option: YahooStockOption, indices: list()):
         self.__stockOption = stock_option
@@ -20,8 +23,7 @@ class IndexComparator(AbstractIndexComparator):
         for a_index in indices[1:]:
             df = df.merge(a_index.HistoricalData, left_index=True, right_index=True)
         self.DataComparator = self.DataComparator.merge(df, left_index=True, right_index=True)
-        #self.DataNorma = (self.DataComparator - self.DataComparator.min()) / (self.DataComparator.max() - self.DataComparator.min())
-        self.DataNorma = 100 * self.DataComparator / self.DataComparator.iloc[0]
+        self.DataNorma = self.DataComparator / self.DataComparator.iloc[0]
         print(self.DataNorma.head())
         plt.figure(figsize=(3 * math.log(stock_option.TimeSpan.MonthCount), 4.5))
         for c in self.DataNorma.columns.values:
@@ -29,6 +31,27 @@ class IndexComparator(AbstractIndexComparator):
 
         plt.title(stock_option.SourceColumn + ' Normalized ' + str(stock_option.TimeSpan.MonthCount) + ' months')
         plt.xlabel(stock_option.TimeSpan.StartDateStr + ' - ' + stock_option.TimeSpan.EndDateStr)
-        plt.ylabel('Base 100 variation since ' + stock_option.TimeSpan.StartDateStr)
+        plt.ylabel('Base 1 variation since ' + stock_option.TimeSpan.StartDateStr)
         plt.legend(loc='upper left', fontsize=10)
+        plt.show()
+        # scale to compare array
+        minMaxScaler = preprocessing.MinMaxScaler(feature_range=(0.0, 100.0))
+        stockArrayScaled = minMaxScaler.fit_transform(self.DataComparator)
+        # scale to compare data frame
+        self.DataScaled = pd.DataFrame(stockArrayScaled, columns=self.DataComparator.columns)
+        print(self.DataScaled.tail())
+        plt.figure(figsize=(3 * math.log(stock_option.TimeSpan.MonthCount), 4.5))
+        for c in self.DataNorma.columns.values:
+            plt.plot(self.DataScaled[c], label=c)
+
+        plt.title(stock_option.SourceColumn + ' Scaled ' + str(stock_option.TimeSpan.MonthCount) + ' months')
+        plt.xlabel(stock_option.TimeSpan.StartDateStr + ' - ' + stock_option.TimeSpan.EndDateStr)
+        plt.ylabel('Base 100 scaled span since ' + stock_option.TimeSpan.StartDateStr)
+        plt.legend(loc='upper left', fontsize=10)
+        plt.show()
+        self.DataSimpleReturnsCorr = self.DataComparator.pct_change(1).corr()
+        # graph correlation
+        plt.subplots(figsize=(math.log(stock_option.TimeSpan.MonthCount), math.log(stock_option.TimeSpan.MonthCount)))
+        s_h_m = sns.heatmap(self.DataSimpleReturnsCorr, cmap="RdYlGn", annot= True, fmt= '.2%') #YlOrRd
+        s_h_m.set_xticklabels(s_h_m.get_xticklabels(), rotation=45, horizontalalignment='right')
         plt.show()
