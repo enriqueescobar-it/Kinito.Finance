1048d35572b94a0d580dc7a001a2ffb642bb566d
enriqueescobar-askida <enrique.escobar@askida.com>
2020-09-07 17:29:40 -0400

    RSI indicator plot

--- Common/TechIndicators/RsiIndicator.py
+++ Common/TechIndicators/RsiIndicator.py
@@ -1,51 +1,72 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from Common.Plotters.TechIndicators.AbstractTechIndicatorPlotter import AbstractTechIndicatorPlotter
 from Common.TechIndicators.AbstractTechIndicator import AbstractTechIndicator
 from Common.StockOptions.Yahoo.YahooStockOption import YahooStockOption
 
 
-class RsiIndicator(AbstractTechIndicator):
-    __avgGain: pd.core.series.Series
-    __avgLoss: pd.core.series.Series
-    __delta: pd.core.series.Series
+class RsiIndicator(AbstractTechIndicator, AbstractTechIndicatorPlotter):
     __period: int
-    __rs: pd.core.series.Series
-    _rsi: pd.core.series.Series
 
     def __init__(self, y_stock_option: YahooStockOption):
         super().__init__(y_stock_option)
         self._name = 'RSI'
         self._label += self._name
-        self.__delta = y_stock_option.HistoricalData[self._col].diff(1)
+        self._main_label += ' ' + self._label
         self.__setPeriod(14)
-        self.__setUp()
-        self.__setDown()
-        self.__setRsi()
-        self._data['Delta'] = self.__delta
-        self._data['AverageGain'] = self.__avgGain
-        self._data['AverageLoss'] = self.__avgLoss
-        self._data['RelativeStrength'] = self.__rs
-        self._data[self._name] = self._rsi
-        print(self._data.tail())
-
-    def __setUp(self):
-        up = self.__delta.copy()
-        up[up < 0] = 0
-        self.__setAvgGain(up)
-
-    def __setDown(self):
-        down = self.__delta.copy()
-        down[down > 0] = 0
-        self.__setAvgLoss(down)
+        self._setData(y_stock_option)
 
     def __setPeriod(self, a_int: int):
         self.__period = a_int
 
-    def __setAvgGain(self, up: pd.core.series.Series):
-        self.__avgGain = up.rolling(window=self.__period).mean()
+    def __getAverageGain(self, delta: pd.core.series.Series) -> pd.core.series.Series:
+        up = delta.copy()
+        up[up < 0] = 0
+        return up.rolling(window=self.__period).mean()
 
-    def __setAvgLoss(self, down: pd.core.series.Series):
-        self.__avgLoss = abs(down.rolling(window=self.__period).mean())
+    def __getAverageLoss(self, delta: pd.core.series.Series) -> pd.core.series.Series:
+        down = delta.copy()
+        down[down > 0] = 0
+        return abs(down.rolling(window=self.__period).mean())
 
     def __setRsi(self):
-        self.__rs = self.__avgGain / self.__avgLoss
         self._rsi = 100.0 - (100.0 / (1.0 + self.__rs))
+
+    def _setData(self, y_stock_option: YahooStockOption):
+        self._data[self._col] = y_stock_option.HistoricalData[self._col]
+        delta: pd.core.series.Series = y_stock_option.HistoricalData[self._col].diff(1)
+        self._data['Delta'] = delta
+        avgGain: pd.core.series.Series = self.__getAverageGain(delta)
+        self._data['AverageGain'] = avgGain
+        avgLoss: pd.core.series.Series = self.__getAverageLoss(delta)
+        self._data['AverageLoss'] = avgLoss
+        rs = avgGain / avgLoss
+        self._data['RelativeStrength'] = rs
+        self._data[self._name] = 100.0 - (100.0 / (1.0 + rs))
+        print(self._data.tail())
+
+    def PlotData(self) -> plt:
+        plt.figure(figsize=self._fig_size)
+        plt.style.use(self._plot_style)
+        colors = cm.coolwarm
+        for a_ind, col in enumerate(self._data.columns[-1:self._data.columns.size]):
+            an_alpha: float = 0.5 if a_ind != 0 else 1.0
+            self._data[col].plot(alpha=an_alpha)
+            print('i', a_ind)
+        plt.axhline(10, linestyle='--', label='10%', alpha=0.50, color='gray')
+        plt.axhline(20, linestyle='--', label='20%', alpha=0.50, color='orange')
+        plt.axhline(30, linestyle='--', label='30%', alpha=0.50, color='green')
+        plt.axhline(40, linestyle='--', label='40%', alpha=0.50, color='red')
+        plt.axhline(60, linestyle='--', label='60%', alpha=0.50, color='red')
+        plt.axhline(70, linestyle='--', label='70%', alpha=0.50, color='green')
+        plt.axhline(80, linestyle='--', label='80%', alpha=0.50, color='orange')
+        plt.axhline(90, linestyle='--', label='90%', alpha=0.50, color='gray')
+        plt.title(self._main_label)
+        plt.xlabel(self._x_label)
+        plt.xticks(rotation=self._x_ticks_angle)
+        plt.ylabel(self._y_label)
+        plt.legend(loc=self._legend_place)
+        plt.grid(True)
+        return plt
--- script-StrategyRsi.py
+++ script-StrategyRsi.py
@@ -1,15 +1,15 @@
-#from Common.Plotters.Strategies.RsiStrategyPlotter import RsiStrategyPlotter
-from Common.Plotters.TechIndicators.RsiIndicatorPlotter import RsiIndicatorPlotter
 from Common.StockOptions.Yahoo.YahooStockOption import YahooStockOption
-#from Common.Strategies.TechIndicators.RsiStrategy import RsiStrategy
 from Common.TechIndicators.RsiIndicator import RsiIndicator
+#from Common.Strategies.TechIndicators.RsiStrategy import RsiStrategy
+#from Common.Plotters.Strategies.RsiStrategyPlotter import RsiStrategyPlotter
 
 yahooStockOption: YahooStockOption = YahooStockOption('CNI')
 print(yahooStockOption.HistoricalData.describe(include='all'))
 yahooStockIndicator: RsiIndicator = RsiIndicator(yahooStockOption)
-print(yahooStockIndicator._label)
-yahooStockIndicatorPlotter: RsiIndicatorPlotter = RsiIndicatorPlotter(yahooStockOption, yahooStockIndicator).Plot()
-yahooStockIndicatorPlotter.show()
+print(yahooStockIndicator.GetLabel())
+yahooStockIndicator.PlotData().show()
+'''yahooStockIndicatorPlotter: RsiIndicatorPlotter = RsiIndicatorPlotter(yahooStockOption, yahooStockIndicator).Plot()
+yahooStockIndicatorPlotter.show()'''
 #yahooStockStrategy: RsiStrategy = RsiStrategy(yahooStockIndicator, yahooStockOption)
 #yahooStockStrategyPlotter: RsiStrategyPlotter = RsiStrategyPlotter(yahooStockOption, yahooStockStrategy).Plot()
 #yahooStockStrategyPlotter.show()
