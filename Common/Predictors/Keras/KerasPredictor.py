dae69a2c69f40cd50a9b66a425b6ee701f424a0f
enriqueescobar-askida <enrique.escobar@askida.com>
2020-09-08 12:16:25 -0400

    update

--- Common/Predictors/Keras/KerasPredictor.py
+++ Common/Predictors/Keras/KerasPredictor.py
@@ -1,5 +1,48 @@
+from numpy.core._multiarray_umath import ndarray
+
 from Common.Predictors.AbstractPredictor import AbstractPredictor
+from Common.Measures.Time.TimeSpan import TimeSpan
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+from pandas import DataFrame
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class KerasPredictor(AbstractPredictor):
-    pass
+    __model: Sequential
+    _name = 'Keras'
+
+    def __init__(self, span: int = 60, a_col: str = 'Adj Close', a_df: DataFrame = DataFrame(), t_s: TimeSpan = TimeSpan()):
+        self.__model = Sequential()
+        self._forward_span = span
+        print(self._label)
+        self._column = self._label + self._name + str(span)
+        print(self._column)
+        self._src_col = a_col
+        self._src_data = a_df
+        self._setData()
+        dataSet: ndarray = self._data[self._src_col].values
+        print(dataSet.shape)
+        dataLength = np.math.ceil(len(dataSet) * 0.8)
+        scale = MinMaxScaler(feature_range=(0, 1))
+        dataScaled = scale.fit_transform(dataSet)
+
+    def _setData(self):
+        print('col_src', self._src_col)
+        print('col_new', self._column)
+        self._data = self._src_data[self._src_col].to_frame()
+        self._data[self._column] = self._src_data[[self._src_col]].shift(-self._forward_span)
+        print('data_src', self._data.shape)
+        print('data_new', self._data.head(1))
+
+    def _setForecast(self):
+        pass
+
+    def _setIndependent(self):
+        pass
+
+    def _setDependent(self):
+        pass
--- script-Predictors.py
+++ script-Predictors.py
@@ -2,9 +2,13 @@ from Common.StockOptions.Yahoo.YahooStockOption import YahooStockOption
 from Common.Predictors.AbstractPredictor import AbstractPredictor
 from Common.Predictors.Tree.DecisionTreePredictor import DecisionTreePredictor
 from Common.Predictors.Linear.LinearPredictor import LinearPredictor
+from Common.Predictors.Svr.LinearSvrPredictor import LinearSvrPredictor
+from Common.Predictors.Svr.PolySvrPredictor import PolySvrPredictor
+from Common.Predictors.Svr.RbfSvrPredictor import RbfSvrPredictor
+from Common.Predictors.Keras.KerasPredictor import KerasPredictor
 
 yahooStockOption: YahooStockOption = YahooStockOption('AAPL')
 print(yahooStockOption.HistoricalData.describe(include='all'))
 predictor_list = []
 abstractPredictor: AbstractPredictor =\
-    LinearPredictor(60, 'Adj Close', yahooStockOption.HistoricalData, yahooStockOption.TimeSpan)
+    KerasPredictor(60, 'Adj Close', yahooStockOption.HistoricalData, yahooStockOption.TimeSpan)
