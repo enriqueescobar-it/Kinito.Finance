0aa76fa4f6185c454e403cccbbf581b9e03a2d3d
enriqueescobar-askida <enrique.escobar@askida.com>
2020-09-10 17:00:05 -0400

    ema

--- script-Ema.py
+++ script-Ema.py
@@ -2,7 +2,7 @@ from Common.StockOptions.Yahoo.YahooStockOption import YahooStockOption
 from Common.TechIndicators.EmaIndicator import EmaIndicator
 from Common.Strategies.TechIndicators.EmaStrategy import EmaStrategy
 
-yahooStockOption: YahooStockOption = YahooStockOption('CNI')
+yahooStockOption: YahooStockOption = YahooStockOption('WCN')
 print(yahooStockOption.HistoricalData.describe(include='all'))
 yahooStockIndicator: EmaIndicator = EmaIndicator(yahooStockOption)
 print(yahooStockIndicator.GetLabel())
