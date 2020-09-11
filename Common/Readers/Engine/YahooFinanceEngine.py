3d1d54336ae3cda671075b23ca95ef94484b2c47
enriqueescobar-askida <enrique.escobar@askida.com>
2020-09-11 00:49:26 -0400

    default values

--- Common/Readers/Engine/YahooFinanceEngine.py
+++ Common/Readers/Engine/YahooFinanceEngine.py
@@ -8,77 +8,84 @@ from Common.Readers.Engine.AbstractEngine import AbstractEngine
 
 class YahooFinanceEngine(AbstractEngine):
     """description of class"""
-    Url: str
-    LogoUrl: str
-    Address: str
-    City: str
-    State: str
-    PostalCode: str
-    Country: str
-    Beta: float
-    Market: str
-    Currency: str
-    QuoteType: str
-    Exchange: str
-    High52: float
-    Low52: float
-    Average50: float
-    Average200: float
-    MarketCap: float
-    PayoutRatio: float
-    PEforward: float
-    PEtrailing: float
-    PegRatio: float
-    ShortRatio: float
-    BookValue: float
-    PriceToBook: float
+    Url: str = 'NA'
+    LogoUrl: str = 'NA'
+    Address: str = 'NA'
+    City: str = 'NA'
+    State: str = 'NA'
+    PostalCode: str = 'NA'
+    Country: str = 'NA'
+    Beta: float = -1.1
+    Market: str = 'NA'
+    Currency: str = 'NA'
+    QuoteType: str = 'NA'
+    Exchange: str = 'NA'
+    High52: float = -1.1
+    Low52: float = -1.1
+    Average50: float = -1.1
+    Average200: float = -1.1
+    MarketCap: float = -1.1
+    PayoutRatio: float = -1.1
+    PEforward: float = -1.1
+    PEtrailing: float = -1.1
+    PegRatio: float = -1.1
+    ShortRatio: float = -1.1
+    BookValue: float = -1.1
+    PriceToBook: float = -1.1
     ExDividendDate: datetime = datetime.date.min
-    __ticker: str
-    InfoDic: dict
-    '''ActionsDf: pd.DataFrame
-    Balance_SheetDf: pd.DataFrame
-    Balance_SheetQDf: pd.DataFrame
-    BalanceSheetDf: pd.DataFrame
-    BalanceSheetQDf: pd.DataFrame
-    CalendarDf: pd.DataFrame
-    CashFlowDf: pd.DataFrame
-    CashFlowQDf: pd.DataFrame
-    EarningDf: pd.DataFrame
-    EarningQDf: pd.DataFrame
-    FinancialDf: pd.DataFrame
-    FinancialQDf: pd.DataFrame
-    IsIn: str
-    OptionTuple: tuple
-    RecommendationDf: pd.DataFrame
+    __ticker: str = 'NA'
+    InfoDic: dict = dict()
+    ActionsDf: pd.DataFrame = pd.DataFrame()
+    Balance_SheetDf: pd.DataFrame = pd.DataFrame()
+    BalanceSheetDf: pd.DataFrame = pd.DataFrame()
+    CalendarDf: pd.DataFrame = pd.DataFrame()
+    CashFlowDf: pd.DataFrame = pd.DataFrame()
+    EarningDf:pd.DataFrame = pd.DataFrame()
+    FinancialDf: pd.DataFrame = pd.DataFrame()
+    IsIn: str = 'NA'
+    Balance_SheetQDf: pd.DataFrame = pd.DataFrame()
+    BalanceSheetQDf: pd.DataFrame = pd.DataFrame()
+    CashFlowQDf: pd.DataFrame = pd.DataFrame()
+    EarningQDf: pd.DataFrame = pd.DataFrame()
+    FinancialQDf: pd.DataFrame = pd.DataFrame()
+    RecommendationDf: pd.DataFrame = pd.DataFrame()
     SplitSeries: Series
-    SustainabilityDf: pd.DataFrame'''
+    SustainabilityDf: pd.DataFrame = pd.DataFrame()
+    OptionTuple: tuple
 
     def __init__(self, a_ticker: str = 'AAPL'):
         self.__ticker = a_ticker
         self.__yFinance = yf.Ticker(a_ticker)
-        print('INFO:', self.__yFinance.__dict__['_info'])
-        if '_info' in self.__yFinance.__dict__:
-            self.__setInfo()
+        if '_info' in self.__yFinance.__dict__ or hasattr(self.__yFinance, '_info'):
+            if self.__yFinance.__dict__['_info'] is not None:
+                self.__setInfo()
         self.ActionsDf = self.__yFinance.actions
-        self.Balance_SheetDf = self.__yFinance.balance_sheet
-        self.BalanceSheetDf = self.__yFinance.balancesheet
-        self.CalendarDf = self.__yFinance.calendar
-        self.CashFlowDf = self.__yFinance.cashflow
-        self.EarningDf = self.__yFinance.earnings
-        self.FinancialDf = self.__yFinance.financials
-        self.IsIn = "NA" if self.__yFinance.isin == "-" else self.__yFinance.isin
-        #print('TYPE', self.__yFinance.options) TYPE ('2020-09-17', '2020-10-15', '2020-11-19', '2020-12-17', '2021-02-18')
-        #self.OptionTuple = self.__yFinance.options#
-        self.Balance_SheetQDf = self.__yFinance.quarterly_balance_sheet
-        self.BalanceSheetQDf = self.__yFinance.quarterly_balancesheet
-        self.CashFlowQDf = self.__yFinance.quarterly_cashflow
-        self.EarningQDf = self.__yFinance.quarterly_earnings
-        self.FinancialQDf = self.__yFinance.quarterly_financials
-        self.RecommendationDf = self.__yFinance.recommendations
+        if '_balance_sheet' in self.__yFinance.__dict__ or hasattr(self.__yFinance, '_balance_sheet'):
+            self.Balance_SheetDf = self.__yFinance.balance_sheet
+        #self.BalanceSheetDf = self.__yFinance.balancesheet
+        #self.CalendarDf = self.__yFinance.calendar
+        #self.CashFlowDf = self.__yFinance.cashflow
+        #self.EarningDf = self.__yFinance.earnings
+        #self.FinancialDf = self.__yFinance.financials
+        #self.IsIn = "NA" if self.__yFinance.isin == "-" else self.__yFinance.isin
+        if '_options' in self.__yFinance.__dict__ or hasattr(self.__yFinance, '_options'):
+            self.OptionTuple = self.__yFinance.options
+        if '_quarterly_balance_sheet' in self.__yFinance.__dict__ or hasattr(self.__yFinance, '_quarterly_balance_sheet'):
+            self.Balance_SheetQDf = self.__yFinance.quarterly_balance_sheet
+        if '_quarterly_balancesheet' in self.__yFinance.__dict__ or hasattr(self.__yFinance, '_quarterly_balancesheet'):
+            self.BalanceSheetQDf = self.__yFinance.quarterly_balancesheet
+        if '_quarterly_cashflow' in self.__yFinance.__dict__ or hasattr(self.__yFinance, '_quarterly_cashflow'):
+            self.CashFlowQDf = self.__yFinance.quarterly_cashflow
+        if '_quarterly_earnings' in self.__yFinance.__dict__ or hasattr(self.__yFinance, '_quarterly_earnings'):
+            self.EarningQDf = self.__yFinance.quarterly_earnings
+        if '_quarterly_financials' in self.__yFinance.__dict__ or hasattr(self.__yFinance, '_quarterly_financials'):
+            self.FinancialQDf = self.__yFinance.quarterly_financials
+        #self.RecommendationDf = self.__yFinance.recommendations
         self.SplitSeries = self.__yFinance.splits
-        self.SustainabilityDf = self.__yFinance.sustainability
+        #self.SustainabilityDf = self.__yFinance.sustainability
 
     def __setInfo(self):
+        print('INFO_2:', self.__yFinance.__dict__['_info'])
         self.InfoDic = self.__yFinance.info
         print('InfoDic\r\n', self.InfoDic)
         self.Url = self.__getValue('website')
