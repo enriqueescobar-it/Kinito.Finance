edce3d44f4f8618ec12847282359062db2b021b8
enriqueescobar-askida <enrique.escobar@askida.com>
2020-09-14 00:02:57 -0400

    update

--- Common/StockOptions/Yahoo/YahooStockOption.py
+++ Common/StockOptions/Yahoo/YahooStockOption.py
@@ -103,32 +103,30 @@ class YahooStockOption(AbstractStockOption):
     YssLink: str
     YssMarketCap: str
     YssPeRatio: str
-    __fin_viz_engine: FinVizEngine
-    __y_finance_engine: YahooFinanceEngine
-    __yahooSummaryScrapper: YahooSummaryScrapper
+    _fin_viz_engine: FinVizEngine
+    _y_finance_engine: YahooFinanceEngine
+    _yahooSummaryScrapper: YahooSummaryScrapper
 
     def __init__(self, a_ticker: str = 'CNI'):
         self.Source = 'yahoo'
         self.SourceColumn = 'Adj Close'
         self.Ticker = a_ticker
         self.TimeSpan = TimeSpan()
-        self.__GetData()
-        self.__GetDataSimpleReturns()
+        self._setData()
+        self._setDataSimpleReturns()
         self._setDataLogReturns()
-        self.__GetDataDaily()
-        self.__GetDataDailyCum()
-        self.__GetDataMonthly()
-        self.__GetDataMonthlyCum()
-        self._setDataMeanRemove()
+        self._setDataDaily()
+        self._setDataMonthly()
+        self._setDataStandard()
         self._setDataScaled()
         self._setDataNormL1()
         self._setDataBinary()
-        self.__GetDataPrediction()
-        self.__GetFv()
-        self.__GetYe()
-        self.__GetYss()
+        self._setDataPrediction()
+        self._setFinViz()
+        self._setYahooFinance()
+        self._setYahooSummary()
 
-    def __GetOutliers(self, a_df: pd.DataFrame, n_sigmas: int = 3):
+    def _getOutliers(self, a_df: pd.DataFrame, n_sigmas: int = 3):
         a_df['IsOutlier'] = pd.Series(dtype=int)
         a_df['Outliers'] = pd.Series(dtype=float)
         for ind in a_df.index:
@@ -140,13 +138,171 @@ class YahooStockOption(AbstractStockOption):
                 a_df['Outliers'][ind] = x
         return a_df
 
-    def __GetData(self):
+    def _setData(self):
         self.HistoricalData = PandaEngine(self.Source, self.TimeSpan, self.Ticker).DataFrame
         self.HistoricalData.fillna(method='ffill', inplace=True)
         self.HistoricalData.fillna(method='bfill', inplace=True)
         # self.HistoricalData.columns = self.Ticker + self.HistoricalData.columns
 
-    def __GetDataPrediction(self):
+    def _setDataSimpleReturns(self):
+        self.HistoricalSimpleReturns = self.HistoricalData[self.SourceColumn].pct_change().to_frame()
+        df_rolling = self.HistoricalSimpleReturns[self.SourceColumn].rolling(window=21).agg(['mean', 'std'])
+        self.HistoricalSimpleReturns = self.HistoricalSimpleReturns.join(df_rolling)
+        self.HistoricalSimpleReturns = self._getOutliers(self.HistoricalSimpleReturns)
+        df = self.HistoricalSimpleReturns[self.SourceColumn].dropna()
+        dfLength = len(df)
+        dfLength80 = int(round(dfLength*0.8))
+        dfLength20 = dfLength - dfLength80
+        train = df[:dfLength80].to_frame()
+        test = df[dfLength80:].to_frame()
+        ##
+        print(train.shape)
+        print(dfLength80)
+        print(test.shape)
+        print(dfLength20)
+        preds = []
+        for i in range(0, test.shape[0]):
+            a = train[self.SourceColumn][len(train)-dfLength20+i:].sum() + sum(preds)
+            b = a/dfLength20
+            preds.append(b)
+        self.RMSE = np.sqrt(np.mean(np.power((np.array(test[self.SourceColumn])-preds), 2)))
+        print('\n RMSE value on validation set:', self.RMSE)
+        lin_svr = SVR(kernel='linear', C=1000.0)
+        #lin_svr.fit(self.HistoricalSimpleReturns.index, self.HistoricalSimpleReturns[self.SourceColumn])
+        ##
+        #T = len(test)
+        #N = len(test)
+        #S_0 = df[train.index[-1]]#.date()]
+        #N_SIM = 100
+        #mu = train.mean()
+        #sigma = train.std()
+        #gbm_simulations: ndarray = self.simulate_gbm(S_0, mu, sigma, N_SIM, T, N)
+        #gbm_simulationsT: ndarray = np.transpose(gbm_simulations)
+        #print('gbm_simulationsT', gbm_simulationsT.shape)
+        # prepare objects for plotting
+        #LAST_TRAIN_DATE = train.index[-1].date()
+        #FIRST_TEST_DATE = test.index[0].date()
+        #LAST_TEST_DATE = test.index[-1].date()
+        #PLOT_TITLE = f'{self.Ticker} Simulation ({FIRST_TEST_DATE}:{LAST_TEST_DATE})'
+        #selected_indices = self.HistoricalData[self.SourceColumn][LAST_TRAIN_DATE:LAST_TEST_DATE].index
+        #a_index = [date.date() for date in selected_indices]
+        #print('a_index', len(a_index))
+        #gbm_simulationsDf = pd.DataFrame(data=gbm_simulationsT, index=a_index)
+        #print('gbm_simulationsDf', gbm_simulationsDf.shape)
+        # plotting
+        #ax = gbm_simulationsDf.plot(alpha=0.2, legend=False)
+        #line_1, = ax.plot(a_index, gbm_simulationsDf.mean(axis=1), color='red')
+        #line_2, = ax.plot(a_index, self.HistoricalData[self.SourceColumn][LAST_TRAIN_DATE:LAST_TEST_DATE], color='blue')
+        #ax.set_title(PLOT_TITLE, fontsize=16)
+        #ax.legend((line_1, line_2), ('mean', 'actual'))
+        #plt.show()
+
+    def _setDataLogReturns(self):
+        a_var = np.log(self.HistoricalData[self.SourceColumn] / self.HistoricalData[self.SourceColumn].shift(1))
+        self.HistoricalLogReturns = a_var.to_frame()
+        self.HistoricalLogReturns['MovingStd252'] =\
+            self.HistoricalLogReturns[self.SourceColumn].rolling(window=252).std().to_frame()
+        self.HistoricalLogReturns['MovingStd21'] =\
+            self.HistoricalLogReturns[self.SourceColumn].rolling(window=21).std().to_frame()
+
+    def _setDataDaily(self):
+        self.HistoricalDaily = self.HistoricalData[self.SourceColumn].pct_change().to_frame()
+        self.HistoricalDailyCum = (self.HistoricalDaily + 1).cumprod()
+
+    def _setDataMonthly(self):
+        self.HistoricalMonthly = self.HistoricalData[self.SourceColumn].resample('M').ffill().pct_change().to_frame()
+        self.HistoricalMonthlyCum = (self.HistoricalMonthly + 1).cumprod()
+
+    def _setDataStandard(self):
+        self.HistoricalStandardized = preprocessing.scale(self.HistoricalData)
+
+    def _setDataScaled(self):
+        self.HistoricalScaled = preprocessing.MinMaxScaler(feature_range=(0, 1)).fit_transform(self.HistoricalData)
+
+    def _setDataNormL1(self):
+        self.HistoricalL1Normalized = preprocessing.normalize(self.HistoricalData, norm='l1')
+
+    def _setDataBinary(self):
+        self.HistoricalBinary = preprocessing.Binarizer(threshold=1.4).transform(self.HistoricalData)
+
+    def _setFinViz(self):
+        self._fin_viz_engine = FinVizEngine(self.Ticker)
+        self.FvCompanyName = self._fin_viz_engine.StockName
+        self.FvCompanySector = self._fin_viz_engine.StockSector
+        self.FvCompanyIndustry = self._fin_viz_engine.StockIndustry
+        self.FvCompanyCountry = self._fin_viz_engine.StockCountry
+        self.FvPeRatio = self._fin_viz_engine.PeRatio
+        self.FvMarketCap = self._fin_viz_engine.MarketCap
+        self.FvEPS = self._fin_viz_engine.EpsTtm
+        self.FvBeta = self._fin_viz_engine.Beta
+        self.FvEarnings = self._fin_viz_engine.EarningDate
+        self.FvLow52 = self._fin_viz_engine.Low52
+        self.FvHigh52 = self._fin_viz_engine.High52
+        self.FvRange52 = self._fin_viz_engine.Range52
+        self.FvRsi14 = self._fin_viz_engine.Rsi14
+        self.FvVolatility = self._fin_viz_engine.Volatility
+        self.FvPayout = self._fin_viz_engine.PayoutPcnt
+        self.FvVolume = self._fin_viz_engine.Volume
+        self.FvChangePercent = self._fin_viz_engine.ChangePcnt
+        self.FvPrice = self._fin_viz_engine.Price
+        self.FvDividend = self._fin_viz_engine.Dividend
+        self.FvDividendPercent = self._fin_viz_engine.DividendPcnt
+
+    def _setYahooFinance(self):
+        self._y_finance_engine = YahooFinanceEngine(self.Ticker)
+        self.YeUrl = self._y_finance_engine.Url
+        self.YeLogoUrl = self._y_finance_engine.LogoUrl
+        self.YeAddress = self._y_finance_engine.Address
+        self.YeCity = self._y_finance_engine.City
+        self.YePostalCode = self._y_finance_engine.PostalCode
+        self.YeState = self._y_finance_engine.State
+        self.YeCountry = self._y_finance_engine.Country
+        self.YeBeta = self._y_finance_engine.Beta
+        self.YeMarket = self._y_finance_engine.Market
+        self.YeCurrency = self._y_finance_engine.Currency
+        self.YeQuoteType = self._y_finance_engine.QuoteType
+        self.YeExchange = self._y_finance_engine.Exchange
+        self.YeCountry = self._y_finance_engine.Country
+        self.YeBeta = self._y_finance_engine.Beta
+        self.YeMarket = self._y_finance_engine.Market
+        self.YeCurrency = self._y_finance_engine.Currency
+        self.YeQuoteType = self._y_finance_engine.QuoteType
+        self.YeExchange = self._y_finance_engine.Exchange
+        self.YeHigh52 = self._y_finance_engine.High52
+        self.YeLow52 = self._y_finance_engine.Low52
+        self.YeAverage50 = self._y_finance_engine.Average50
+        self.YeAverage200 = self._y_finance_engine.Average200
+        self.YeMarketCap = self._y_finance_engine.MarketCap
+        self.YePayoutRatio = self._y_finance_engine.PayoutRatio
+        self.YePeForward = self._y_finance_engine.PEforward
+        self.YePeTrailing = self._y_finance_engine.PEtrailing
+        self.YePegRatio = self._y_finance_engine.PegRatio
+        self.YeShortRatio = self._y_finance_engine.ShortRatio
+        self.YeBookValue = self._y_finance_engine.BookValue
+        self.YePriceToBook = self._y_finance_engine.PriceToBook
+        self.YeExDividendDate = self._y_finance_engine.ExDividendDate
+
+    def _setYahooSummary(self):
+        self._yahooSummaryScrapper = YahooSummaryScrapper(self.Ticker)
+        self._yahooSummaryScrapper.ParseBody()
+        self.YssPeRatio = self._yahooSummaryScrapper.PEratio
+        self.YssMarketCap = self._yahooSummaryScrapper.MarketCap
+        self.YssEPS = self._yahooSummaryScrapper.EPS
+        self.YssBeta = self._yahooSummaryScrapper.Beta
+        self.YssEarningsDate = self._yahooSummaryScrapper.EarningsDate
+        self.YssLink = self._yahooSummaryScrapper.Link
+
+    def simulate_gbm(self, s_0, mu, sigma, n_sims, T, N):
+        dt = T/N
+        dW = np.random.normal(scale = np.sqrt(dt), size=(n_sims, N))
+        W = np.cumsum(dW, axis=1)
+        time_step = np.linspace(dt, T, N)
+        time_steps = np.broadcast_to(time_step, (n_sims, N))
+        S_t = s_0 * np.exp((mu - 0.5 * sigma ** 2) * time_steps + sigma * W)
+        S_t = np.insert(S_t, 0, s_0, axis=1)
+        return S_t
+
+    def _setDataPrediction(self):
         #into the future
         self.ForecastSpan = 30
         #TREE
@@ -213,165 +369,3 @@ class YahooStockOption(AbstractStockOption):
         model.add(Dense(25))
         model.add(Dense(1))
         exit(-111)'''
-
-    def __GetDataSimpleReturns(self):
-        self.HistoricalSimpleReturns = self.HistoricalData[self.SourceColumn].pct_change().to_frame()
-        df_rolling = self.HistoricalSimpleReturns[self.SourceColumn].rolling(window=21).agg(['mean', 'std'])
-        self.HistoricalSimpleReturns = self.HistoricalSimpleReturns.join(df_rolling)
-        self.HistoricalSimpleReturns = self.__GetOutliers(self.HistoricalSimpleReturns)
-        df = self.HistoricalSimpleReturns[self.SourceColumn].dropna()
-        dfLength = len(df)
-        dfLength80 = int(round(dfLength*0.8))
-        dfLength20 = dfLength - dfLength80
-        train = df[:dfLength80].to_frame()
-        test = df[dfLength80:].to_frame()
-        ##
-        print(train.shape)
-        print(dfLength80)
-        print(test.shape)
-        print(dfLength20)
-        preds = []
-        for i in range(0, test.shape[0]):
-            a = train[self.SourceColumn][len(train)-dfLength20+i:].sum() + sum(preds)
-            b = a/dfLength20
-            preds.append(b)
-        self.RMSE = np.sqrt(np.mean(np.power((np.array(test[self.SourceColumn])-preds), 2)))
-        print('\n RMSE value on validation set:', self.RMSE)
-        lin_svr = SVR(kernel='linear', C=1000.0)
-        #lin_svr.fit(self.HistoricalSimpleReturns.index, self.HistoricalSimpleReturns[self.SourceColumn])
-        ##
-        #T = len(test)
-        #N = len(test)
-        #S_0 = df[train.index[-1]]#.date()]
-        #N_SIM = 100
-        #mu = train.mean()
-        #sigma = train.std()
-        #gbm_simulations: ndarray = self.simulate_gbm(S_0, mu, sigma, N_SIM, T, N)
-        #gbm_simulationsT: ndarray = np.transpose(gbm_simulations)
-        #print('gbm_simulationsT', gbm_simulationsT.shape)
-        # prepare objects for plotting
-        #LAST_TRAIN_DATE = train.index[-1].date()
-        #FIRST_TEST_DATE = test.index[0].date()
-        #LAST_TEST_DATE = test.index[-1].date()
-        #PLOT_TITLE = f'{self.Ticker} Simulation ({FIRST_TEST_DATE}:{LAST_TEST_DATE})'
-        #selected_indices = self.HistoricalData[self.SourceColumn][LAST_TRAIN_DATE:LAST_TEST_DATE].index
-        #a_index = [date.date() for date in selected_indices]
-        #print('a_index', len(a_index))
-        #gbm_simulationsDf = pd.DataFrame(data=gbm_simulationsT, index=a_index)
-        #print('gbm_simulationsDf', gbm_simulationsDf.shape)
-        # plotting
-        #ax = gbm_simulationsDf.plot(alpha=0.2, legend=False)
-        #line_1, = ax.plot(a_index, gbm_simulationsDf.mean(axis=1), color='red')
-        #line_2, = ax.plot(a_index, self.HistoricalData[self.SourceColumn][LAST_TRAIN_DATE:LAST_TEST_DATE], color='blue')
-        #ax.set_title(PLOT_TITLE, fontsize=16)
-        #ax.legend((line_1, line_2), ('mean', 'actual'))
-        #plt.show()
-
-    def simulate_gbm(self, s_0, mu, sigma, n_sims, T, N):
-        dt = T/N
-        dW = np.random.normal(scale = np.sqrt(dt), size=(n_sims, N))
-        W = np.cumsum(dW, axis=1)
-        time_step = np.linspace(dt, T, N)
-        time_steps = np.broadcast_to(time_step, (n_sims, N))
-        S_t = s_0 * np.exp((mu - 0.5 * sigma ** 2) * time_steps + sigma * W)
-        S_t = np.insert(S_t, 0, s_0, axis=1)
-        return S_t
-
-    def _setDataLogReturns(self):
-        a_var = np.log(self.HistoricalData[self.SourceColumn] / self.HistoricalData[self.SourceColumn].shift(1))
-        self.HistoricalLogReturns = a_var.to_frame()
-        self.HistoricalLogReturns['MovingStd252'] =\
-            self.HistoricalLogReturns[self.SourceColumn].rolling(window=252).std().to_frame()
-        self.HistoricalLogReturns['MovingStd21'] =\
-            self.HistoricalLogReturns[self.SourceColumn].rolling(window=21).std().to_frame()
-
-    def __GetDataDaily(self):
-        self.HistoricalDaily = self.HistoricalData[self.SourceColumn].pct_change().to_frame()
-
-    def __GetDataDailyCum(self):
-        self.HistoricalDailyCum = (self.HistoricalDaily + 1).cumprod()
-
-    def __GetDataMonthly(self):
-        self.HistoricalMonthly = self.HistoricalData[self.SourceColumn].resample('M').ffill().pct_change().to_frame()
-
-    def __GetDataMonthlyCum(self):
-        self.HistoricalMonthlyCum = (self.HistoricalMonthly + 1).cumprod()
-
-    def _setDataMeanRemove(self):
-        self.HistoricalStandardized = preprocessing.scale(self.HistoricalData)
-
-    def _setDataScaled(self):
-        self.HistoricalScaled = preprocessing.MinMaxScaler(feature_range=(0, 1)).fit_transform(self.HistoricalData)
-
-    def _setDataNormL1(self):
-        self.HistoricalL1Normalized = preprocessing.normalize(self.HistoricalData, norm='l1')
-
-    def _setDataBinary(self):
-        self.HistoricalBinary = preprocessing.Binarizer(threshold=1.4).transform(self.HistoricalData)
-
-    def __GetFv(self):
-        self.__fin_viz_engine = FinVizEngine(self.Ticker)
-        self.FvCompanyName = self.__fin_viz_engine.StockName
-        self.FvCompanySector = self.__fin_viz_engine.StockSector
-        self.FvCompanyIndustry = self.__fin_viz_engine.StockIndustry
-        self.FvCompanyCountry = self.__fin_viz_engine.StockCountry
-        self.FvPeRatio = self.__fin_viz_engine.PeRatio
-        self.FvMarketCap = self.__fin_viz_engine.MarketCap
-        self.FvEPS = self.__fin_viz_engine.EpsTtm
-        self.FvBeta = self.__fin_viz_engine.Beta
-        self.FvEarnings = self.__fin_viz_engine.EarningDate
-        self.FvLow52 = self.__fin_viz_engine.Low52
-        self.FvHigh52 = self.__fin_viz_engine.High52
-        self.FvRange52 = self.__fin_viz_engine.Range52
-        self.FvRsi14 = self.__fin_viz_engine.Rsi14
-        self.FvVolatility = self.__fin_viz_engine.Volatility
-        self.FvPayout = self.__fin_viz_engine.PayoutPcnt
-        self.FvVolume = self.__fin_viz_engine.Volume
-        self.FvChangePercent = self.__fin_viz_engine.ChangePcnt
-        self.FvPrice = self.__fin_viz_engine.Price
-        self.FvDividend = self.__fin_viz_engine.Dividend
-        self.FvDividendPercent = self.__fin_viz_engine.DividendPcnt
-
-    def __GetYe(self):
-        self.__y_finance_engine = YahooFinanceEngine(self.Ticker)
-        self.YeUrl = self.__y_finance_engine.Url
-        self.YeLogoUrl = self.__y_finance_engine.LogoUrl
-        self.YeAddress = self.__y_finance_engine.Address
-        self.YeCity = self.__y_finance_engine.City
-        self.YePostalCode = self.__y_finance_engine.PostalCode
-        self.YeState = self.__y_finance_engine.State
-        self.YeCountry = self.__y_finance_engine.Country
-        self.YeBeta = self.__y_finance_engine.Beta
-        self.YeMarket = self.__y_finance_engine.Market
-        self.YeCurrency = self.__y_finance_engine.Currency
-        self.YeQuoteType = self.__y_finance_engine.QuoteType
-        self.YeExchange = self.__y_finance_engine.Exchange
-        self.YeCountry = self.__y_finance_engine.Country
-        self.YeBeta = self.__y_finance_engine.Beta
-        self.YeMarket = self.__y_finance_engine.Market
-        self.YeCurrency = self.__y_finance_engine.Currency
-        self.YeQuoteType = self.__y_finance_engine.QuoteType
-        self.YeExchange = self.__y_finance_engine.Exchange
-        self.YeHigh52 = self.__y_finance_engine.High52
-        self.YeLow52 = self.__y_finance_engine.Low52
-        self.YeAverage50 = self.__y_finance_engine.Average50
-        self.YeAverage200 = self.__y_finance_engine.Average200
-        self.YeMarketCap = self.__y_finance_engine.MarketCap
-        self.YePayoutRatio = self.__y_finance_engine.PayoutRatio
-        self.YePeForward = self.__y_finance_engine.PEforward
-        self.YePeTrailing = self.__y_finance_engine.PEtrailing
-        self.YePegRatio = self.__y_finance_engine.PegRatio
-        self.YeShortRatio = self.__y_finance_engine.ShortRatio
-        self.YeBookValue = self.__y_finance_engine.BookValue
-        self.YePriceToBook = self.__y_finance_engine.PriceToBook
-        self.YeExDividendDate = self.__y_finance_engine.ExDividendDate
-
-    def __GetYss(self):
-        self.__yahooSummaryScrapper = YahooSummaryScrapper(self.Ticker)
-        self.__yahooSummaryScrapper.ParseBody()
-        self.YssPeRatio = self.__yahooSummaryScrapper.PEratio
-        self.YssMarketCap = self.__yahooSummaryScrapper.MarketCap
-        self.YssEPS = self.__yahooSummaryScrapper.EPS
-        self.YssBeta = self.__yahooSummaryScrapper.Beta
-        self.YssEarningsDate = self.__yahooSummaryScrapper.EarningsDate
-        self.YssLink = self.__yahooSummaryScrapper.Link
--- script-Portfolio.py
+++ script-Portfolio.py
@@ -1,6 +1,6 @@
 from Common.Comparators.Portfolio.PortfolioComparator import PortfolioComparator
 from Common.StockOptions.Yahoo.YahooStockOption import YahooStockOption
-
+#from typing import List
 yahooTickers: list = ['CNI', 'CP', 'TD', 'OTEX']
 yahooStocks: list = list()
 for yahooTicker in yahooTickers:
