e5f592e0899bbeb21d37e97538e8cc0dd27d91dd
enriqueescobar-askida <enrique.escobar@askida.com>
2020-09-24 14:58:51 -0400

    abstract stock update

---
+++ Common/Stock/AbstractStock.py
@@ -0,0 +1,5 @@
+from abc import *
+
+
+class AbstractStock(ABC):
+    pass
---
+++ Common/Stock/Funds/AbstractStockFund.py
@@ -0,0 +1,6 @@
+from abc import *
+from Common.Stock.AbstractStock import AbstractStock
+
+
+class AbstractStockFund(AbstractStock):
+    pass
