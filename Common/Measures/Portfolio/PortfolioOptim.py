b3be6f85e794e138b910114634aa0585c597f69e
enriqueescobar-askida <enrique.escobar@askida.com>
2020-10-14 13:20:19 -0400

    global vars

--- Common/Measures/Portfolio/PortfolioOptim.py
+++ Common/Measures/Portfolio/PortfolioOptim.py
@@ -5,41 +5,49 @@ import matplotlib.pyplot as plt
 
 class PortfolioOptim(AbstractPortfolioMeasure):
     _threshold: int = 5000
+    _weight_matrix: np.ndarray
+    _return_matrix: np.ndarray
+    _risk_matrix: np.ndarray
+    _sharpe_ratio_matrix: np.ndarray
 
     def __init__(self, portfolio_data: DataFrame = DataFrame(), log_ret: DataFrame = DataFrame(),
                  cov_mat: DataFrame = DataFrame()):
         # Creating an empty array to store portfolio weights
-        all_wts: np.ndarray = np.zeros((self._threshold, len(portfolio_data.columns)))
+        self._weight_matrix = np.zeros((self._threshold, len(portfolio_data.columns)))
         # Creating an empty array to store portfolio returns
-        port_returns: np.ndarray = np.zeros(self._threshold)
+        self._return_matrix = np.zeros(self._threshold)
         # Creating an empty array to store portfolio risks
-        port_risk: np.ndarray = np.zeros(self._threshold)
+        self._risk_matrix = np.zeros(self._threshold)
         # Creating an empty array to store portfolio sharpe ratio
-        sharpe_ratio: np.ndarray = np.zeros(self._threshold)
+        self._sharpe_ratio_matrix = np.zeros(self._threshold)
+        self._setMatrices(portfolio_data, log_ret, cov_mat)
+
+    def _setMatrices(self, portfolio_data: DataFrame, log_ret: DataFrame, cov_mat: DataFrame):
         for i in range(self._threshold):
             wts: np.ndarray = np.random.uniform(size=len(portfolio_data.columns))
             wts = wts / np.sum(wts)
             # saving weights in the array
-            all_wts[i, :] = wts
+            self._weight_matrix[i, :] = wts
             # Portfolio Returns
             port_ret: float = np.sum(log_ret.mean() * wts)
             port_ret = (port_ret + 1) ** 252 - 1
             # Saving Portfolio returns
-            port_returns[i] = port_ret
+            self._return_matrix[i] = port_ret
             # Saving Portfolio Risk
             port_sd: float = np.sqrt(np.dot(wts.T, np.dot(cov_mat, wts)))
-            port_risk[i] = port_sd
+            self._risk_matrix[i] = port_sd
             # Portfolio Sharpe Ratio
             # Assuming 0% Risk Free Rate
             sr: float = port_ret / port_sd
-            sharpe_ratio[i] = sr
+            self._sharpe_ratio_matrix[i] = sr
         col_names = portfolio_data.columns
-        min_var: np.ndarray = all_wts[port_risk.argmin()]
+        print('col_names', col_names)
+        min_var: np.ndarray = self._weight_matrix[self._risk_matrix.argmin()]
         print('min_var', min_var)
-        max_sr: np.ndarray = all_wts[sharpe_ratio.argmax()]
+        max_sr: np.ndarray = self._weight_matrix[self._sharpe_ratio_matrix.argmax()]
         print('max_sr', max_sr)
-        print('sharpe_ratio', sharpe_ratio.max())
-        print('port_risk', port_risk.min())
+        print('sharpe_ratio', self._sharpe_ratio_matrix.max())
+        print('port_risk', self._risk_matrix.min())
         min_var: Series = Series(min_var, index=col_names)
         min_var = min_var.sort_values()
         fig = plt.figure()
@@ -65,5 +73,5 @@ class PortfolioOptim(AbstractPortfolioMeasure):
         ax1.set_xlabel('Risk')
         ax1.set_ylabel('Returns')
         ax1.set_title('Portfolio optimization and Efficient Frontier')
-        plt.scatter(port_risk, port_returns)
+        plt.scatter(self._risk_matrix, self._return_matrix)
         plt.show()
