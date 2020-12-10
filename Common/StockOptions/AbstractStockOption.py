751dd1987a804672e9876e37c8c72258f43641ff
enriqueescobar-askida <enrique.escobar@askida.com>
2020-12-10 00:35:59 -0500

    update

--- Common/StockOptions/AbstractStockOption.py
+++ Common/StockOptions/AbstractStockOption.py
@@ -4,4 +4,11 @@ from pandas import DataFrame
 
 class AbstractStockOption(ABC):
     Source: str = ''
-    Data: DataFrame
+    _data: DataFrame
+
+    def getData(self) -> DataFrame:
+        return self._data
+
+    @property
+    def Data(self):
+        return self._data
