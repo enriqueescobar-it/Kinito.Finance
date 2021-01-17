97637804373a6d7e7babdbc985b9b8d8e2639c44
enriqueescobar-askida <enrique.escobar@askida.com>
2021-01-17 15:20:32 -0500

    currency baseline

--- Common/StockType/Currencies/Crypto/CryptoCurrency.py
+++ Common/StockType/Currencies/Crypto/CryptoCurrency.py
@@ -1,16 +1,8 @@
-from prettytable import PrettyTable
-
 from Common.StockType.Currencies.AbstractCurrency import AbstractCurrency
 
 
 class CryptoCurrency(AbstractCurrency):
-    __pretty_table: PrettyTable = PrettyTable()
 
     def __init__(self, c_name: str):
         super().__init__(c_name.replace(' ', '').replace('-', '|'))
         self.__class = 'Crypto'
-        self.__pretty_table.add_column('Labels', self.InfoLabels)
-        self.__pretty_table.add_column(self.__class, self.InfoList)
-
-    def __str__(self):
-        return self.__pretty_table.__str__()
