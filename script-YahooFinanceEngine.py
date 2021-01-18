1321c8c63704143a26a96486e854ead5813ec7a7
enriqueescobar-askida <enrique.escobar.it@gmail.com>
2021-01-17 23:54:14 -0500

    price to book

--- script-YahooFinanceEngine.py
+++ script-YahooFinanceEngine.py
@@ -1,4 +1,22 @@
 from Common.Readers.Engine.YahooFinanceEngine import YahooFinanceEngine
+import requests
+
+api_key = '41da2fad6cc1f1221dc6b74b0a926139'
+
+
+def getpricetobook(stock):
+    BS = requests.get(
+        f"https://financialmodelingprep.com/api/v3/balance-sheet-statement/{stock}?period=quarter&limit=400&apikey={api_key}")
+    BS = BS.json()
+    book_value_equity_now = float(BS[0]['totalStockholdersEquity'])
+    print('book_value_equity_now', book_value_equity_now)
+    company_info = requests.get(f"https://financialmodelingprep.com/api/v3/profile/{stock}?apikey={api_key}")
+    company_info = company_info.json()
+    market_cap = float(company_info[0]['mktCap'])
+    print('market_cap', market_cap)
+    price_to_book = market_cap/book_value_equity_now
+    print('price_to_book', price_to_book)
+    print(BS)
 
 tech_stocks = ['AAPL', 'MSFT', 'INTC', 'ATD-B.TO']
 bank_stocks = ['WFC', 'BAC', 'C']
@@ -8,7 +26,8 @@ commodity_futures = ['GC=F', 'SI=F', 'CL=F']
 etf_stocks = ['VOO', 'VOOG', 'GINN.TO', 'VGRO.TO', 'XIT.TO']
 mutual_funds = ['PRLAX', 'QASGX', 'HISFX']
 us_treasuries = ['^TNX', '^IRX', '^TYX']
-a_ticker: str = 'VOOG'
+a_ticker: str = 'WFC'
+getpricetobook(a_ticker)
 abstractEngine: YahooFinanceEngine = YahooFinanceEngine(a_ticker)
 print(abstractEngine)
 print(abstractEngine.StockType)
