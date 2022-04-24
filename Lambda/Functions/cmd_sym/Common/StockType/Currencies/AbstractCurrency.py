75871c14783c1ec251149e497740d4065c84c963
Enrique Escobar <enrique.escobar.it@gmail.com>
2022-04-23 20:54:20 -0400

    update

--- Lambda/Functions/cmd_sym/Common/StockType/Currencies/AbstractCurrency.py
+++ Lambda/Functions/cmd_sym/Common/StockType/Currencies/AbstractCurrency.py
@@ -7,12 +7,6 @@ import numpy as np
 class AbstractCurrency(AbstractStock):
     __ticker: str = 'NA'
     _name: str = 'NA'
-    _stock_part_count: int = -1
-    _bond_part_count: int = -1
-    _price_to_earn: float = np.nan
-    _price_to_book: float = np.nan
-    _price_to_sale: float = np.nan
-    _price_to_cash: float = np.nan
 
     def __init__(self, c_name: str):
         self._name = c_name.replace(' ', '')
@@ -27,12 +21,6 @@ class AbstractCurrency(AbstractStock):
         pt.add_row(['ticker', self.__ticker])
         pt.add_row(['type', self.__class])
         pt.add_row(['name', self._name])
-        pt.add_row(['StockPartCount', self._stock_part_count])
-        pt.add_row(['BondPartCount', self._bond_part_count])
-        pt.add_row(['PriceToEarnings', self._price_to_earn])
-        pt.add_row(['PriceToBook', self._price_to_book])
-        pt.add_row(['PriceToSales', self._price_to_sale])
-        pt.add_row(['PriceToCashflow', self._price_to_cash])
         return pt.__str__()
 
     def __repr__(self):
