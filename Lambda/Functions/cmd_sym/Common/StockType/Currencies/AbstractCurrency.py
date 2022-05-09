17d05a8f6368ecc9c45eddfa27bc4764f6429f6c
Enrique Escobar <enrique.escobar.it@gmail.com>
2022-05-09 17:22:15 -0400

    currency tipe

--- Lambda/Functions/cmd_sym/Common/StockType/Currencies/AbstractCurrency.py
+++ Lambda/Functions/cmd_sym/Common/StockType/Currencies/AbstractCurrency.py
@@ -1,12 +1,21 @@
+from pandas import DataFrame
 from prettytable import PrettyTable
+import numpy as np
 
 from Common.StockType.AbstractStock import AbstractStock
-import numpy as np
 
 
 class AbstractCurrency(AbstractStock):
     __ticker: str = 'NA'
     _name: str = 'NA'
+    _sector_df: DataFrame = DataFrame()
+    _holding_df: DataFrame = DataFrame()
+    _stock_part_count: int = -1
+    _bond_part_count: int = -1
+    _price_to_earn: float = np.nan
+    _price_to_book: float = np.nan
+    _price_to_sale: float = np.nan
+    _price_to_cash: float = np.nan
 
     def __init__(self, c_name: str):
         self._name = c_name.replace(' ', '')
@@ -14,6 +23,8 @@ class AbstractCurrency(AbstractStock):
         self.__class = 'Currency'
         #
         self._setInfo()
+        self._sector_df = DataFrame(columns=['S'])
+        self._holding_df = DataFrame(columns=['H'])
 
     def __str__(self):
         pt: PrettyTable = PrettyTable()
@@ -21,6 +32,12 @@ class AbstractCurrency(AbstractStock):
         pt.add_row(['ticker', self.__ticker])
         pt.add_row(['type', self.__class])
         pt.add_row(['name', self._name])
+        pt.add_row(['StockPartCount', self._stock_part_count])
+        pt.add_row(['BondPartCount', self._bond_part_count])
+        pt.add_row(['PriceToEarnings', self._price_to_earn])
+        pt.add_row(['PriceToBook', self._price_to_book])
+        pt.add_row(['PriceToSales', self._price_to_sale])
+        pt.add_row(['PriceToCashflow', self._price_to_cash])
         return pt.__str__()
 
     def __repr__(self):
@@ -41,3 +58,35 @@ class AbstractCurrency(AbstractStock):
     @property
     def Name(self):
         return self._name
+
+    @property
+    def SectorDataFrame(self):
+        return self._sector_df
+
+    @property
+    def HoldingDataFrame(self):
+        return self._holding_df
+
+    @property
+    def StockPartCount(self):
+        return self._stock_part_count
+
+    @property
+    def BondPartCount(self):
+        return self._bond_part_count
+
+    @property
+    def PriceToEarnings(self):
+        return self._price_to_earn
+
+    @property
+    def PriceToSales(self):
+        return self._price_to_sale
+
+    @property
+    def PriceToBook(self):
+        return self._price_to_book
+
+    @property
+    def PriceToCashflow(self):
+        return self._price_to_cash
--- Lambda/Functions/cmd_sym/Common/StockType/Currencies/Crypto/CryptoCurrency.py
+++ Lambda/Functions/cmd_sym/Common/StockType/Currencies/Crypto/CryptoCurrency.py
@@ -1,28 +1,35 @@
-import numpy as np
-from pandas import DataFrame
+from prettytable import PrettyTable
+
 from Common.StockType.Currencies.AbstractCurrency import AbstractCurrency
 
 
 class CryptoCurrency(AbstractCurrency):
-    _sector_df: DataFrame = DataFrame()
-    _holding_df: DataFrame = DataFrame()
-    _stock_part_count: int = -1
-    _bond_part_count: int = -1
-    _price_to_earn: float = np.nan
-    _price_to_book: float = np.nan
-    _price_to_sale: float = np.nan
-    _price_to_cash: float = np.nan
 
     def __init__(self, c_name: str, t_name: str):
         super().__init__(c_name.replace(' ', '').replace('-', ''))
         self.__class = 'Crypto'
         self.__ticker = t_name
 
+    def __str__(self):
+        pt: PrettyTable = PrettyTable()
+        pt.field_names = self._header
+        pt.add_row(['ticker', self.__ticker])
+        pt.add_row(['type', self.__class])
+        pt.add_row(['name', self._name])
+        pt.add_row(['StockPartCount', self._stock_part_count])
+        pt.add_row(['BondPartCount', self._bond_part_count])
+        pt.add_row(['PriceToEarnings', self._price_to_earn])
+        pt.add_row(['PriceToBook', self._price_to_book])
+        pt.add_row(['PriceToSales', self._price_to_sale])
+        pt.add_row(['PriceToCashflow', self._price_to_cash])
+        return pt.__str__()
+
     def __iter__(self):
         yield from {
+            "Info": "StockInfo",
+            "ticker": self.__ticker,
             "type": self.__class,
             "name": self._name,
-            "ticker": self.__ticker,
             "stock_percent": self._stock_part_count,
             "bond_percent": self._bond_part_count,
             "price_to_earnings": self._price_to_earn,
@@ -30,35 +37,3 @@ class CryptoCurrency(AbstractCurrency):
             "price_to_sales": self._price_to_sale,
             "price_to_cashflow": self._price_to_cash
         }.items()
-
-    @property
-    def SectorDataFrame(self):
-        return self._sector_df
-
-    @property
-    def HoldingDataFrame(self):
-        return self._holding_df
-
-    @property
-    def StockPartCount(self):
-        return self._stock_part_count
-
-    @property
-    def BondPartCount(self):
-        return self._bond_part_count
-
-    @property
-    def PriceToEarnings(self):
-        return self._price_to_earn
-
-    @property
-    def PriceToSales(self):
-        return self._price_to_sale
-
-    @property
-    def PriceToBook(self):
-        return self._price_to_book
-
-    @property
-    def PriceToCashflow(self):
-        return self._price_to_cash
