5d21cf3b758c224364058bb81255b776158173d3
Enrique Escobar <enrique.escobar.it@gmail.com>
2022-05-20 00:44:09 -0400

    in funds

--- Lambda/Functions/cmd_sym/test_9_mutual_fund.py
+++ Lambda/Functions/cmd_sym/test_9_mutual_fund.py
@@ -1,19 +1,51 @@
+from Common.InfoType.stock_info import stock_info
 from Common.StockType.AbstractStock import AbstractStock
 from Common.StockType.Funds.AbstractStockFund import AbstractStockFund
 from Common.StockType.Funds.Mutual.MutualFund import MutualFund
 
+print('\n============   ', 'abstractStock', '   ============\n')
 abstractStock: AbstractStock = AbstractStock()
 print(abstractStock)
 print(abstractStock.to_json())
 
+print('\n============   ', 'abstractStockFund', '   ============\n')
 abstractStockFund: AbstractStockFund = AbstractStockFund('stock fund', 'A_QUOTE')
 print(abstractStockFund)
 print(abstractStockFund.to_json())
 
+print('\n============   ', 'mf_0', '   ============\n')
 mf_0: MutualFund = MutualFund('a mutual', 'QASGX', 'UN_QUOTE')
 print(mf_0)
 print(mf_0.to_json())
 
+print('\n============   ', 'mf_1', '   ============\n')
 mf_1: MutualFund = MutualFund('un mutual', 'HISFX', 'UNE_QUOTE')
 print(mf_1)
 print(mf_1.to_json())
+
+print('\n============   ', 'mutual_stock_info', '   ============\n')
+mutual_stock_info: stock_info = stock_info('QASGX')
+print(mutual_stock_info)
+print(mutual_stock_info.to_json())
+print(mutual_stock_info.CompanyName)
+print(mutual_stock_info.QuoteType)
+print(mutual_stock_info.StockType)
+mutual_stock_info.set_actions()
+print(mutual_stock_info.ActionDataFrame.info())
+print(mutual_stock_info.ActionDataFrame.describe())
+print(mutual_stock_info.ActionDataFrame.columns)
+print(mutual_stock_info.ActionDataFrame.dtypes)
+print(mutual_stock_info.BalanceSheetDataFrame.info())
+print(mutual_stock_info.BalanceSheetDataFrame.describe())
+print(mutual_stock_info.BalanceSheetDataFrame.columns)
+print(mutual_stock_info.BalanceSheetDataFrame.dtypes)
+print(mutual_stock_info.QuarterCashflowDataFrame.info())
+print(mutual_stock_info.QuarterCashflowDataFrame.describe())
+print(mutual_stock_info.QuarterCashflowDataFrame.columns)
+print(mutual_stock_info.QuarterCashflowDataFrame.dtypes)
+print(mutual_stock_info.QuarterFinancialDataFrame.head())
+print(mutual_stock_info.QuarterBalanceSheetDataFrame.head())
+print(mutual_stock_info.OptionTuple)
+mutual_stock_info.set_splits()
+print(mutual_stock_info.SplitSeries)
+print(mutual_stock_info.SplitSeries.dtypes)
