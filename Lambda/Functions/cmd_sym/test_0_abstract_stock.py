2611e506b01d45697f60726b30dc60989365fc09
Enrique Escobar <enrique.escobar.it@gmail.com>
2022-05-26 11:22:29 -0400

    update

--- Lambda/Functions/cmd_sym/test_0_abstract_stock.py
+++ Lambda/Functions/cmd_sym/test_0_abstract_stock.py
@@ -1,9 +1,9 @@
 from Common.StockType.AbstractStock import AbstractStock
 
 abstractStock: AbstractStock = AbstractStock()
-print(abstractStock)
 print(abstractStock.to_json())
+print(abstractStock)
 
 aStock: AbstractStock = AbstractStock()
-print(aStock)
 print(aStock.to_json())
+print(aStock)
