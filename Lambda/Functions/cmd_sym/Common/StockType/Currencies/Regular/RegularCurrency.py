0b4e7e307dcacc3dad4afd6b01bb506979a58f19
Enrique Escobar <enrique.escobar.it@gmail.com>
2022-05-30 13:41:04 -0400

    oups

--- Lambda/Functions/cmd_sym/Common/StockType/Currencies/Regular/RegularCurrency.py
+++ Lambda/Functions/cmd_sym/Common/StockType/Currencies/Regular/RegularCurrency.py
@@ -1,24 +1,35 @@
-from prettytable import PrettyTable
+import json
 
+import matplotlib.pyplot as plt
+import numpy as np
+from pandas import DataFrame
+import pandas
+from prettytable import PrettyTable
+from yahooquery import Ticker
 from Common.StockType.Currencies.AbstractCurrency import AbstractCurrency
 
 
 class RegularCurrency(AbstractCurrency):
+    __y_query: Ticker
 
     def __init__(self, c_name: str, t_name: str, q_type: str):
         super().__init__(c_name.replace(' ', '').replace('/', '-'), q_type)
         self.__class = 'Regular'
         self.__ticker = t_name
+        self.__y_query = Ticker(t_name)
+        print(type(self.__y_query.fund_sector_weightings))
+        print(self.__y_query.fund_sector_weightings)
 
     def __str__(self):
         pt: PrettyTable = PrettyTable()
         pt.field_names = self._header
+        pt.add_row(['Info', 'StockInfo'])
         pt.add_row(['Ticker', self.__ticker])
         pt.add_row(['Type', self.__class])
         pt.add_row(['QuoteType', self._quote_type])
         pt.add_row(['Name', self._name])
-        pt.add_row(['StockPartCount', self._stock_part_count])
-        pt.add_row(['BondPartCount', self._bond_part_count])
+        pt.add_row(['StockPercent', self._stock_part_count])
+        pt.add_row(['BondPercent', self._bond_part_count])
         pt.add_row(['PriceToEarnings', self._price_to_earn])
         pt.add_row(['PriceToBook', self._price_to_book])
         pt.add_row(['PriceToSales', self._price_to_sale])
