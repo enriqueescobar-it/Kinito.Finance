2d5f7c1d553adafc5dea903b1bf282a4c45f412f
Enrique Escobar <enrique.escobar.it@gmail.com>
2022-05-29 22:09:33 -0400

    test 5

--- Lambda/Functions/cmd_sym/test_5_crypto_currency.py
+++ Lambda/Functions/cmd_sym/test_5_crypto_currency.py
@@ -1,4 +1,3 @@
-from Common.InfoType.stock_info import stock_info
 from Common.StockType.AbstractStock import AbstractStock
 from Common.StockType.Currencies.AbstractCurrency import AbstractCurrency
 from Common.StockType.Currencies.Crypto.CryptoCurrency import CryptoCurrency
@@ -22,28 +21,3 @@ print('\n============   ', 'c_Currency', '   ============\n')
 c_Currency: CryptoCurrency = CryptoCurrency('an xrp', 'XRP-USD', 'UN_QUOTE')
 print(c_Currency.to_json())
 print(c_Currency)
-
-print('\n============   ', 'crypto_stock_info', '   ============\n')
-crypto_stock_info: stock_info = stock_info('ETH-USD')
-print(crypto_stock_info.to_json())
-print(crypto_stock_info)
-exit(111)
-print(crypto_stock_info.CompanyName)
-print(crypto_stock_info.QuoteType)
-print(crypto_stock_info.ActionDataFrame.info())
-print(crypto_stock_info.ActionDataFrame.describe())
-print(crypto_stock_info.ActionDataFrame.columns)
-print(crypto_stock_info.ActionDataFrame.dtypes)
-print(crypto_stock_info.BalanceSheetDataFrame.info())
-print(crypto_stock_info.BalanceSheetDataFrame.describe())
-print(crypto_stock_info.BalanceSheetDataFrame.columns)
-print(crypto_stock_info.BalanceSheetDataFrame.dtypes)
-print(crypto_stock_info.QuarterCashflowDataFrame.info())
-print(crypto_stock_info.QuarterCashflowDataFrame.describe())
-print(crypto_stock_info.QuarterCashflowDataFrame.columns)
-print(crypto_stock_info.QuarterCashflowDataFrame.dtypes)
-print(crypto_stock_info.QuarterFinancialDataFrame.head())
-print(crypto_stock_info.QuarterBalanceSheetDataFrame.head())
-print(crypto_stock_info.OptionTuple)
-print(crypto_stock_info.SplitSeries)
-print(crypto_stock_info.SplitSeries.dtypes)
