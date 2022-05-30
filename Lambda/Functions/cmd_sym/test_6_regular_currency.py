2c48eb790ad7d311c989854b8c9cfb1a735fe918
Enrique Escobar <enrique.escobar.it@gmail.com>
2022-05-29 22:12:15 -0400

    test 6

--- Lambda/Functions/cmd_sym/test_6_regular_currency.py
+++ Lambda/Functions/cmd_sym/test_6_regular_currency.py
@@ -1,4 +1,3 @@
-from Common.InfoType.stock_info import stock_info
 from Common.StockType.AbstractStock import AbstractStock
 from Common.StockType.Currencies.AbstractCurrency import AbstractCurrency
 from Common.StockType.Currencies.Regular.RegularCurrency import RegularCurrency
@@ -22,28 +21,3 @@ print('\n============   ', 'r_Currency', '   ============\n')
 r_Currency: RegularCurrency = RegularCurrency('a canada', 'CDN=X', 'UNE_QUOTE')
 print(r_Currency.to_json())
 print(r_Currency)
-
-print('\n============   ', 'currency_stock_info', '   ============\n')
-currency_stock_info: stock_info = stock_info('CDN=X')
-print(currency_stock_info.to_json())
-print(currency_stock_info)
-exit(111)
-print(currency_stock_info.CompanyName)
-print(currency_stock_info.QuoteType)
-print(currency_stock_info.ActionDataFrame.info())
-#print(currency_stock_info.ActionDataFrame.describe())
-print(currency_stock_info.ActionDataFrame.columns)
-print(currency_stock_info.ActionDataFrame.dtypes)
-print(currency_stock_info.BalanceSheetDataFrame.info())
-print(currency_stock_info.BalanceSheetDataFrame.describe())
-print(currency_stock_info.BalanceSheetDataFrame.columns)
-print(currency_stock_info.BalanceSheetDataFrame.dtypes)
-print(currency_stock_info.QuarterCashflowDataFrame.info())
-print(currency_stock_info.QuarterCashflowDataFrame.describe())
-print(currency_stock_info.QuarterCashflowDataFrame.columns)
-print(currency_stock_info.QuarterCashflowDataFrame.dtypes)
-print(currency_stock_info.QuarterFinancialDataFrame.head())
-print(currency_stock_info.QuarterBalanceSheetDataFrame.head())
-print(currency_stock_info.OptionTuple)
-print(currency_stock_info.SplitSeries)
-print(currency_stock_info.SplitSeries.dtypes)
