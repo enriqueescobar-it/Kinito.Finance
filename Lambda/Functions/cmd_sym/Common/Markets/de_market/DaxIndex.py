c2ebff677658930662278ff5ea4a7f9e948a4f6c
enriqueescobar-askida <enrique.escobar.it@gmail.com>
2022-06-29 17:54:57 -0400

    markets

--- Lambda/Functions/cmd_sym/Common/Markets/de_market/DaxIndex.py
+++ Lambda/Functions/cmd_sym/Common/Markets/de_market/DaxIndex.py
@@ -2,4 +2,11 @@ from Common.StockType.Funds.Index.IndexFund import IndexFund
 
 
 class DaxIndex(IndexFund):
-    pass
+
+    def __init__(self, source: str = 'yahoo', ticker: str = "^GDAXI") -> None:
+        a_ticker: str = "^GDAXI" if source == 'yahoo' else ticker
+        a_column: str = 'Adj Close' if source == 'yahoo' else ticker
+        a_name: str = 'DeutscherAktien' if source == 'yahoo' else ticker
+        a_to_usd: float = 1.15
+        #super().__init__(source, a_name, a_column, a_ticker, a_to_usd)
+        super().__init__(a_name, a_ticker, 'IndexFund')
