df2c522ba085d4a2493eddc0e216d6570278739f
enriqueescobar-askida <enrique.escobar.it@gmail.com>
2022-07-09 17:37:22 -0400

    AbstractTimeInfo

--- Lambda/Functions/cmd_sym/Common/InfoType/Times/AbstractTimeInfo.py
+++ Lambda/Functions/cmd_sym/Common/InfoType/Times/AbstractTimeInfo.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 
+from prettytable import PrettyTable
+
 from Common.InfoType.AbstractInfo import AbstractInfo
 
 
 class AbstractTimeInfo(AbstractInfo):
     _stop_dt: datetime = datetime.now()
+    _header: list = ['Field', 'FieldInfo']
+    _pretty_table: PrettyTable = PrettyTable()
 
     @property
     def stop_datetime(self) -> datetime:
